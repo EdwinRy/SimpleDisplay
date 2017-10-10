3230848ae7afbfc26805a8d315f13e6839918e43
EdwinRybarczyk <edwinrybarczyk4d@gmail.com>
2017-10-10 14:18:59 +0100

    ok

--- bin/test.py
+++ bin/test.py
@@ -1,4 +1,8 @@
 from ctypes import *
 import os
 ##print(os.environ)
-print(cdll.LoadLibrary(str(os.getcwd())+"/SimpleDisplay.so"))
+window = cdll.LoadLibrary(str(os.getcwd())+"/SimpleDisplay.so")
+window.openDisplay(c_int(800),c_int(600));
+while window.isWindowClosed():
+    window.updateDisplay();
+window.closeDisplay();
